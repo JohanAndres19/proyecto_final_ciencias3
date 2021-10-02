2d1ad9006cd4fa4ecdfb1c877f2bb409f115eecf
JohanAndres19 <johanandresa@hotmail.com>
2021-10-02 12:23:43 -0500

    cambios johan rama master

--- generador_de_codigo.py
+++ generador_de_codigo.py
@@ -1,4 +1,4 @@
-from os import mkdir
+from os import mkdir, name
 from os.path import exists, dirname, join
 import jinja2
 from entity_test import get_entity_mm
@@ -12,54 +12,34 @@ def main(debug=False):
     calculator_model = entity_mm.model_from_file(join(this_folder,'calculadora.en'))
 
     def is_variable(n):
-
         if n in calculator_model.variable:
             return True
         else:
             return False
 
-    def is_command(n):
-
-        if n in calculator_model.command:
-            return True
-        else:
-            return False
-
-    def is_suma(n):
-
-        if n in calculator_model.scomandos:
-            return True
-        else:
-            return False
-
-    def is_resta(n):
-
-        if n in calculator_model.rcomandos:
-            return True
-        else:
-            return False
-
-    def is_multi(n):
-
-        if n in calculator_model.mcomandos:
-            return True
-        else:
-            return False
-
-    def is_division(n):
-
-        if n in calculator_model.dcomandos:
-            return True
-        else:
-            return False
-
     def is_main(n):
-
-        if n in calculator_model.main:
+        if n.name == 'main':
             return True
         else:
-            return False
+            return False       
+    
+    def javatype(s):
+        """
+        Maps type names from SimpleType to Java.
+        """
+        return {
+                'INT': 'int',
+                'void': 'void'
+        }.get(s.name, s.name)
 
+    def is_constructor(n):
+        if n.name =='constructor':
+            return True
+        else:
+            return False  
+    
+    def tamaño(n):
+        return len(n)
     # Create output folder
     srcgen_folder = join(this_folder, 'srcgen')
     if not exists(srcgen_folder):
@@ -71,19 +51,17 @@ def main(debug=False):
         trim_blocks=True,
         lstrip_blocks=True)
 
+    
     # Register filter for mapping Entity type names to Java type names.
 
     #jinja_env.tests['entity'] = is_entity
     
     #jinja_env.filters['javatype'] = javatype
-
+    jinja_env.filters['javatype'] = javatype
+    jinja_env.filters['tamaño']= tamaño
     jinja_env.tests['variable'] = is_variable
-    jinja_env.tests['command'] = is_command
-    jinja_env.tests['suma'] = is_suma
-    jinja_env.tests['resta'] = is_resta
-    jinja_env.tests['multi'] = is_multi
-    jinja_env.tests['division'] = is_division
     jinja_env.tests['main'] = is_main
+    jinja_env.tests['constructor']=is_constructor
     template = jinja_env.get_template('class.template')
 
     
--- srcgen/Calculadora.java
+++ srcgen/Calculadora.java
@@ -1,33 +1,36 @@
 public class Calculadora {
-      private int x;
-      private int y;
+  
+  private int x;
+  
+  private int y;
 
-      public Calculadora(int x,int y,int z){
-            this.x = x;
-            this.y = y;
-      }
+  public Calculadora(int x , int y){
+    this.x=x;
+    this.y=y;
+  }
+  
+  
+  public void sumar(){
 
-      public void sumar(){
-        System.out.println(x + y + 0);
-      }
+  }
+  
+  
+  public void restar(){
 
-      public void restar(){
-        System.out.println(x - y - 0);
-      }
+  }
+  
+  
+  public void multiplicar(){
 
-      public void multiplicar(){
-        System.out.println(x * y * 1);
-      }
+  }
+  
+  
+  public void division(){
 
-      public void division(){
-        System.out.println(x / y / 1);
-      }
-
-      public static void main(String[] args) {
-        Calculadora a = new Calculadora(10,5,0);
-        a.sumar();
-        a.restar();
-        a.multiplicar();
-        a.division();
-      }
+  }
+  
+  public static void main(String[] args) {
+    new Calculadora(4,5);        
+  }
+  
 }
